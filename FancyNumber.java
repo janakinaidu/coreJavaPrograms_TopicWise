89063ded3972520f235f908dcbd3ff752b6acd9c
JANAKI NAIDU PUNNANA <janakinaidu493@gmail.com>
2019-04-29 23:23:16 +0530

    FancyNumber Code updated
    fancy number Code updated

--- FancyNumber.java
+++ FancyNumber.java
@@ -17,7 +17,7 @@ public class FancyNumber {
 	}
 
 	private static boolean isFancy(String str) {
-		if (cond1(str) || cond2(str) || cond3(str)) {
+		if (condition1(str) || condition2(str) || condition3(str)) {
 			return true;
 		}
 		return false;
@@ -33,11 +33,11 @@ public class FancyNumber {
 	 * 
 	 * Condition3:
 	 *  A single digit occurs four or more times in the number. Like
-	 * 9859009976 – here the digit 9 occurs 4 times.
+	 * 9859009976 â€“ here the digit 9 occurs 4 times.
 	 */
 	
 	//777 //888
-	private static boolean cond1(String str) {
+	private static boolean condition1(String str) {
 		System.out.println("in_condition1");
 		for (int i = 0; i < str.length() - 2; i++) {
 			if (str.charAt(i) == str.charAt(i + 1) &&
@@ -49,7 +49,7 @@ public class FancyNumber {
 	}
 
 	//456  //987
-	private static boolean cond2(String str) {
+	private static boolean condition2(String str) {
 		System.out.println("in_condition2");
 		for (int i = 0; i < str.length()-2; i++) {
 			if((str.charAt(i)<str.charAt(i+1) && str.charAt(i+1)<str.charAt(i+2)) ||
@@ -61,11 +61,12 @@ public class FancyNumber {
 	}
 	
 	//998888522546
-	private static boolean cond3(String str) {
+	private static boolean condition3(String str) {
 		System.out.println("in_condition3");
 		int a[] = new int[10];
 		for (int i = 0; i < str.length(); i++) {
-			a[str.charAt(i)-'0']++;
+			// a[str.charAt(i)-'0']++; // 57-48=9 ASCII codes of 9 and 0; a[9] index position get incremented;
+			a[Character.getNumericValue(str.charAt(i))]++;
 			
 		}
 		for (int i = 0; i < str.length(); i++) {
