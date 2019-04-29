e4039cb43d403607254b954e81d564722101ad10
JANAKI NAIDU PUNNANA <janakinaidu493@gmail.com>
2019-04-29 23:13:16 +0530

    Code for FancyMobile Number and NoOfoccuranceofcharacter
    Code for FancyMobile Number and NoOfoccuranceofcharacter programs

---
+++ FancyNumber.java
@@ -0,0 +1,79 @@
+package programs;
+
+import java.util.Scanner;
+
+public class FancyNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String mobilenumber = sc.next();
+		String str = String.valueOf(mobilenumber);
+		if (isFancy(str)) {
+			System.out.println("The given number is Fancy Number");
+		} else {
+			System.out.println("The given number is not a fancy number");
+		}
+		sc.close();
+	}
+
+	private static boolean isFancy(String str) {
+		if (cond1(str) || cond2(str) || cond3(str)) {
+			return true;
+		}
+		return false;
+	}
+	
+
+	/*Condition1:
+	 * A single number occurs three consecutive times. Like 777.
+	 * 
+	 * Condition2:
+	 *  Three consecutive digits are either in increasing or decreasing fashion. Like
+	 * 456 or 987.
+	 * 
+	 * Condition3:
+	 *  A single digit occurs four or more times in the number. Like
+	 * 9859009976 – here the digit 9 occurs 4 times.
+	 */
+	
+	//777 //888
+	private static boolean cond1(String str) {
+		System.out.println("in_condition1");
+		for (int i = 0; i < str.length() - 2; i++) {
+			if (str.charAt(i) == str.charAt(i + 1) &&
+					str.charAt(i + 1) == str.charAt(i + 2)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	//456  //987
+	private static boolean cond2(String str) {
+		System.out.println("in_condition2");
+		for (int i = 0; i < str.length()-2; i++) {
+			if((str.charAt(i)<str.charAt(i+1) && str.charAt(i+1)<str.charAt(i+2)) ||
+					(str.charAt(i)>str.charAt(i+1) && str.charAt(i+1)>str.charAt(i+2))){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//998888522546
+	private static boolean cond3(String str) {
+		System.out.println("in_condition3");
+		int a[] = new int[10];
+		for (int i = 0; i < str.length(); i++) {
+			a[str.charAt(i)-'0']++;
+			
+		}
+		for (int i = 0; i < str.length(); i++) {
+			if(a[i]>=4){
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
---
+++ NoOfOccurenceOfCharacters.java
@@ -0,0 +1,42 @@
+package programs;
+
+public class NoOfOccurenceOfCharacters {
+
+	static final int MAX_CHAR = 256;
+
+	public static void main(String[] args) {
+		// Scanner sc = new Scanner(System.in);
+		String str = "janakinaidu";
+		getOccuringChar(str);
+	}
+
+	public static void getOccuringChar(String str) {
+
+		// Create an array of size 256 i.e. ASCII_SIZE
+		int count[] = new int[MAX_CHAR];
+
+		int len = str.length();
+
+		// Initialize count array index
+		for (int i = 0; i < len; i++)
+			count[str.charAt(i)]++;
+		
+		System.out.println(count[0]);
+		// Create an array of given String size
+		char ch[] = new char[str.length()];
+		for (int i = 0; i < len; i++) {
+			ch[i] = str.charAt(i);
+			int find = 0;
+			for (int j = 0; j <= i; j++) {
+
+				// If any matches found
+				if (str.charAt(i) == ch[j])
+					find++;
+			}
+			System.out.println(count['j']);
+			if (find == 1)
+				System.out.println("Number of Occurrence of " + str.charAt(i)
+						+ " is:" + count[str.charAt(i)]);
+		}
+	}
+}
